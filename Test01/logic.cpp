fc291be6d813723c416c061d5262066e8adf914e
ITSTEP\st <st>
2024-03-21 18:39:11 +0300

    smth

---
+++ Test01/logic.cpp
@@ -0,0 +1,3 @@
+int sum(int a, int b, int c) {
+	return a + b + c;
+}
--- Test01/main.cpp
+++ Test01/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#define SIZE 10
 
 using namespace std;
+int sum(int a, int b, int c);
 
 int main() {
+	int x = 7;
+	int y = 10;
+	int z = 20;
+
+	cout << sum(x, y, z) << endl;
 
 	return 0;
 }
